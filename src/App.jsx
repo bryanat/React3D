942a038fd12bf958e040d2d2ada1bf3f3547b7c4
bryanat <boyettbryan@gmail.com>
2023-03-09 23:58:23 -0500

    feat: Render 3D .fbx file via threejs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import logo from './Assets/logo.svg'
 import './Assets/App.css';
 import Scene1 from './Scenes/Scene1'
-import MyScene from './Scenes/MyScene'
 
 function App() {
   return (
     <div className="App">
-      <MyScene />
+      <Scene1 />
     </div>
   );
 }
--- src/Scenes/Scene1.jsx
+++ src/Scenes/Scene1.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
-import logo from '../Assets/logo.svg';
+import React, { useRef, useEffect } from 'react';
+import * as THREE from 'three';
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
 import '../Assets/Scene1.css';
 
 export default function Scene1() {
-  return (
-    <div >
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Scene 1</p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const mount = useRef(null);
 
+  useEffect(() => {
+    // create a Three.js scene
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    mount.current.appendChild(renderer.domElement);
+    camera.position.set(0, 0, 10);
+
+    // load the FBX model
+    const loader = new FBXLoader();
+    loader.load('../Assets/biniso20.fbx', function (object) {
+      scene.add(object);
+    });
+
+    // animate the scene
+    function animate() {
+      requestAnimationFrame(animate);
+      renderer.render(scene, camera);
+    }
+    animate();
+  }, []);
+
+  return <div ref={mount} />;
+}

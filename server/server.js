32d1311fc637cef2606ecd9cdd17b73ea0935138
bryanat <boyettbryan@gmail.com>
2023-03-23 23:06:55 -0400

    feat: Add initial server endpoint with cors enabled.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,27 @@
-import express from 'express'
+const express = require('express');
+const cors = require('cors');
 
 // create express app
 const app = new express();
 
+// use cors middleware
+app.use(cors())
+
 // use json middleware
 app.use(express.json());
+
+// test route
+// app.post('/sendtounity', async (req, res) => {
+app.post('/sendtounity', async (req, res) => {
+    console.log('/sendtounity route hitt');
+    console.log(req.body);
+    res.send(req.body);
+
+    // 1 get req from react app with json data
+    // 2 save to json file
+    // 3 run mlagents-learn with json file
+
+})
 
 // start express server on port 8080
 app.listen(8080);

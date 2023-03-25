f66805db4d07cf70fc0de3ca51f2285ab60c762c
bryanat <boyettbryan@gmail.com>
2023-03-25 01:57:09 -0400

    feat: Add tensorboard graphs via iframe.

--- src/Scenes/SceneTensor.jsx
+++ src/Scenes/SceneTensor.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import { FbxContext } from "./FbxContext";
+import React from 'react';
+import { FbxContext } from './FbxContext';
 
 export default function SceneTensor() {
-  const { fbxfilename, setfbxfilename } = React.useContext(FbxContext)
+  const { fbxfilename, setfbxfilename } = React.useContext(FbxContext);
+
   return (
-    <div>SceneTensor ... fbxfilename: {fbxfilename}</div>
-  )
+    <div style={{ width: '100vw', height: '100vh' }}>
+      {/* SceneTensor ... fbxfilename: {fbxfilename} */}
+      <iframe src="http://localhost:6006" title="Example" style={{ width: '100%', height: '100%' }} />
+    </div>
+  );
 }

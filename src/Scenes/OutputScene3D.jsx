63acac94b9829983af93ef40cd01e37c56f098c6
bryanat <boyettbryan@gmail.com>
2023-03-30 00:59:46 -0400

    upgrade: Limit min and max distance for camera in 3D viewer.

--- src/Scenes/OutputScene3D.jsx
+++ src/Scenes/OutputScene3D.jsx
@@ -1,24 +1,22 @@
-import React, { Suspense, useContext, useTransition } from "react";
+import React, { Suspense, useRef, useContext, useTransition } from "react";
 import { FbxContext } from "./FbxContext";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, useFBX } from "@react-three/drei";
+import { OrbitControls, useFBX, PerspectiveCamera } from "@react-three/drei";
 
 export default function OutputScene3D(props) {
   const { fbxfilename, setfbxfilename } = React.useContext(FbxContext)
   const fbx = useFBX(fbxfilename);
 
+  const cameraRef = useRef();
+
   return (
     <Suspense fallback={null}>
       <Canvas >
+        <PerspectiveCamera makeDefault ref={cameraRef} position={[-3, 8, 30]} fov={75} near={0.1} far={1000} />
+        <OrbitControls minDistance={22} maxDistance={44} camera={cameraRef.current} />
         <pointLight position={[1, 1, 1]} />
         <ambientLight />
         <primitive object={fbx} scale={0.005} />;
-        <OrbitControls 
-          makeDefault
-          position={[500, 0.9, 1.8]}
-          fov={60}
-          zoom={0.9}
-        />
       </Canvas>
     </Suspense>
   );

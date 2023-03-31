5f650b14f1646dd32912aabf6c1dbe578d3ad177
bryanat <boyettbryan@gmail.com>
2023-03-31 18:49:35 -0400

    hotfix: Fix styling on 3D canvas to match margins of page.

--- src/Scenes/OutputScene3D.jsx
+++ src/Scenes/OutputScene3D.jsx
@@ -2,6 +2,7 @@ import React, { Suspense, useRef, useContext, useTransition } from "react";
 import { FbxContext } from "./FbxContext";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, useFBX, PerspectiveCamera } from "@react-three/drei";
+import { Box } from "@mui/material";
 
 export default function OutputScene3D(props) {
   const { fbxfilename, setfbxfilename } = React.useContext(FbxContext)
@@ -10,6 +11,8 @@ export default function OutputScene3D(props) {
   const cameraRef = useRef();
 
   return (
+    <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+  <div style={{ flex: '1', margin: '0 22px' }}>
     <Suspense fallback={null}>
       <Canvas >
         <PerspectiveCamera makeDefault ref={cameraRef} position={[-3, 8, 30]} fov={75} near={0.1} far={1000} />
@@ -19,5 +22,7 @@ export default function OutputScene3D(props) {
         <primitive object={fbx} scale={0.005} />;
       </Canvas>
     </Suspense>
+    </div>
+    </div>
   );
 }
